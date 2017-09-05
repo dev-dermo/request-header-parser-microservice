14d24046cbd54a1b6b076140943e70c3829587c4
Diarmuid Murphy <diarmuid.com@gmail.com>
2017-09-04 21:52:05 -0700

    first working version

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('<p>Visit <a href="whoami">/whoami</a> to get your details.</p>');
+});
+
+app.get('/whoami', function(req, res) {
+	var result = {};
+	result.ipaddress = req.ip;
+	result.language = req.headers['accept-language'].split(',')[0];
+	var ua = req.headers['user-agent'];
+	ua = ua.substring(ua.indexOf('(') + 1, ua.indexOf(')'));
+	result.software = ua;
+
+	res.send(result);
+});
+
+app.listen(8080, function() {
+	console.log('App listening on port 8080');
+});
